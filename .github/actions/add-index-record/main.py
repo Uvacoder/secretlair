1634af4ad1abd690643cac1889d529fff9f9c8e2
Chuck Meyer <chuck.meyer@algolia.com>
2021-11-15 18:03:43 -0500

    Remove action from this repo (moved to its own repo)

--- .github/actions/add-index-record/main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from algoliasearch.search_client import SearchClient
-
-client = SearchClient.create(os.environ.get("INPUT_APP_ID"), os.environ.get("INPUT_API_KEY"))
-index = client.init_index(os.environ.get("INPUT_INDEX_NAME"))
-record = os.environ.get("INPUT_INDEX_FILE_PATH"))
-
-index.save_object(record,  {'autoGenerateObjectIDIfNotExist': True})
